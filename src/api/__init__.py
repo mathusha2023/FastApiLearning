8088c24d4901417ceec529a650f6fb4bec523910
Alexey <bazooka2008@inbox.ru>
2025-12-17 11:32:50 +0300

    ssss

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from src.api.cats import router as cats_router
-from src.api.cats_images import router as cats_images_router
+from src.api.routes.cats import router as cats_router
+from src.api.routes.cats_images import router as cats_images_router
 
 main_router = APIRouter(prefix="/api")
 main_router.include_router(cats_router)
